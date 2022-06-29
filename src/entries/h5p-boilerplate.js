d0d5c1bf240284047afe95ce124f3453a1051e20
Zouhair Loucif <zouhair.loucif@gmail.com>
2022-06-29 13:49:32 +0200

    JI-3673 Cleanup for packages and replaced deprecated packages

---
+++ src/entries/h5p-boilerplate.js
@@ -0,0 +1,6 @@
+import "../styles/h5p-boilerplate.css";
+import HelloWorld from "../scripts/h5p-boilerplate";
+
+// Load library
+H5P = H5P || {};
+H5P.HelloWorld = HelloWorld;
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,34 @@
 const path = require('path');
-const webpack = require('webpack');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const MinifyPlugin = require("babel-minify-webpack-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const libraryName = process.env.npm_package_name;
 
 const nodeEnv = process.env.NODE_ENV || 'development';
 const isDev = (nodeEnv !== 'production');
 
 const config = {
-  mode: 'development',
+  mode: nodeEnv,
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    dist: `/entries/${libraryName}.js`
+  },
+  devtool: isDev ? 'inline-source-map' : undefined,
   optimization: {
     minimizer: [
       new OptimizeCSSAssetsPlugin({})
     ]
   },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'h5p-hello-world.css'
-    }),
-    new MinifyPlugin({}, {
-      sourceMap: isDev
-    })
-  ],
-  entry: {
-    dist: './src/entries/h5p-hello-world.js'
-  },
   output: {
-    filename: 'h5p-hello-world.js',
+    filename: `${libraryName}.js`,
     path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
       {
         test: /\.js$/,
+        exclude: /node_modules/,
         loader: 'babel-loader',
-        query: {
-          presets: ['@babel/env']
-        }
       },
       {
         test: /\.css$/,
@@ -48,22 +40,32 @@ const config = {
       {
         test: /\.svg|\.jpg|\.png$/,
         include: path.join(__dirname, 'src/images'),
-        loader: 'file-loader?name=images/[name].[ext]'
+        loader: 'file-loader',
+        options: {
+          name: 'images/[name].[ext]'
+        }
       },
       {
         test: /\.woff$/,
         include: path.join(__dirname, 'src/fonts'),
-        loader: 'file-loader?name=fonts/[name].[ext]'
+        loader: 'file-loader',
+        options: {
+          name: 'fonts/[name].[ext]'
+        }
       }
     ]
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: `${libraryName}.css`
+    }),
+    new MinifyPlugin({}, {
+      sourceMap: isDev
+    })
+  ],
   stats: {
     colors: true
   }
 };
-
-if (isDev) {
-  config.devtool = 'cheap-module-eval-source-map';
-}
 
 module.exports = config;
